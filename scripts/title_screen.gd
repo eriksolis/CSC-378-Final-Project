b228379242ad3f6f127749fb2ce250ea0dd9bdb3
Andrea Ng <andreang0323@gmail.com>
2025-02-27 16:18:41 -0800

    settings menu

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,7 +1,12 @@
 extends Control
 @onready var game = load("res://scenes/game.tscn")
+@onready var musicVolume = AudioServer.get_bus_index("Music")
+@onready var sfxVolume = AudioServer.get_bus_index("SFX")
 var loadedGame
 
+func _ready() -> void:
+	$SettingsMenu.hide()
+	$CreditsPanel.hide()
 
 func _on_start_button_pressed() -> void:
 	loadedGame = game.instantiate()
@@ -22,3 +27,30 @@ func restart():
 func title():
 	loadedGame.queue_free()
 	show()
+
+func _on_settings_button_pressed() -> void:
+	$SettingsMenu/MenuMargin/MenuOptions/MusicContainer/MusicSlider.value = db_to_linear(AudioServer.get_bus_volume_db(musicVolume))
+	$SettingsMenu/MenuMargin/MenuOptions/SFXContainer/SFXSlider.value = db_to_linear(AudioServer.get_bus_volume_db(sfxVolume))
+	$SettingsMenu.show()
+
+func _on_credits_button_pressed() -> void:
+	$CreditsPanel.show()
+
+
+func _on_return_button_pressed() -> void:
+	$SettingsMenu.hide()
+	$CreditsPanel.hide()
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(musicVolume, linear_to_db(value))
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(sfxVolume, linear_to_db(value))
+
+
+func _on_fullscreen_checkbox_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
