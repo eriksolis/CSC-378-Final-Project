74e81ff5c1477a00e2d575755083235a28e3e737
eriksolis <eriksolisschool@gmail.com>
2025-03-05 15:48:26 -0800

    Room1, Scene Switching
    Added room1 in the left of overworld
    Added a scenemanager to switch scenes with entrance markers
    Scene switching is bugged if done multiple times
    Interacting with NPC inside not complete

---
+++ scripts/scene_manager.gd
@@ -0,0 +1,14 @@
+class_name SceneManager extends Node
+
+var player: Player
+
+var scene_dir_path = "res://scenes/"
+
+func change_scene(from, to_scene_name: String) -> void:
+	player = from.get_node("Player")
+	# bugged when going back and forth from scene to scene, "Cannot call method 'get_parent' on a null value"
+	player.get_parent().remove_child(player) 
+	
+	var full_path = scene_dir_path + to_scene_name + ".tscn"
+	from.get_tree().call_deferred("change_scene_to_file", full_path)
+	
