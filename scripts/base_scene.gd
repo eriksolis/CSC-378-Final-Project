05916f04083ad34814dee77ee99b38465ffb47af
eriksolis <eriksolisschool@gmail.com>
2025-03-21 12:50:37 -0700

    the jankiest ending scene ever

--- scripts/base_scene.gd
+++ scripts/base_scene.gd
@@ -13,13 +13,17 @@ func set_player(player):
 		1:
 			for entrance in entrance_markers.get_children():
 				#print(entrance)
-				#print(scene_manager.lastRoom)
+				print(scene_manager.lastRoom)
 				if entrance is Marker2D and entrance.name == "DoorPos" and scene_manager.lastRoom != "room_2":
 					print("doorpos1")
 					player.global_position = entrance.global_position
 					break
 				elif entrance is Marker2D and entrance.name == "DoorPos2" and scene_manager.lastRoom == "room_2":
 					print("doorpos2")
+					player.global_position = entrance.global_position
+					break
+				elif entrance is Marker2D and entrance.name == "DoorPos3":
+					print("ending")
 					player.global_position = entrance.global_position
 					break
 	add_child(player)
