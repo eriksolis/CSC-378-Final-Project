190919daa24b40aebf6d30fd5f605aff1d1035bd
Andrea Ng <andreang0323@gmail.com>
2025-03-21 07:08:25 -0700

    boss healthbars

--- scripts/astrologist_slime.gd
+++ scripts/astrologist_slime.gd
@@ -11,10 +11,10 @@ var moving = true
 var counter = 2
 
 func _ready() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($CanvasLayer/HealthBar, "modulate:a", 1, 0.25)
 	$SummonAnim.play("fadein")
 	await $SummonAnim.animation_finished
-	
-	var player = get_tree().get_first_node_in_group("Player")
  
 
 func destroy():
@@ -23,6 +23,8 @@ func destroy():
 		set_deferred("monitorable", false)
 		set_deferred("monitoring", false)
 		call_deferred("set_physics_process", false)
+		var tween = get_tree().create_tween()
+		tween.tween_property($CanvasLayer/HealthBar, "modulate:a", 0, 0.25)
 		var astrologist = astrologistLoad.instantiate()
 		astrologist.global_position = global_position
 		get_parent().add_child(astrologist)
@@ -84,3 +86,21 @@ func _on_timer_timeout() -> void:
 			counter = 0
 	moving = true
 	$Timer.start()
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("PlayerBullets"):
+		$Hit.pitch_scale = randf_range(0.95, 1)
+		$Hit.play()
+		# modulate flash trick taken from
+		# https://www.reddit.com/r/godot/comments/y8n1wa/is_it_possible_to_make_a_sprite_flash_white_using/
+		$SlimeSprite.modulate.v = 3
+		if tween:
+			tween.kill()
+		tween = get_tree().create_tween()
+		tween.tween_property($SlimeSprite, "modulate:v", 1, 0.2)
+		area.destroy()
+		health -= area.damage
+		$CanvasLayer/HealthBar.value = health
+		if health <= 0:
+			destroy()
--- scripts/king_slime.gd
+++ scripts/king_slime.gd
@@ -1,6 +1,10 @@
 extends "res://scripts/slime.gd"
 
 func _ready() -> void:
+	$CanvasLayer/HealthBar.max_value = health
+	$CanvasLayer/HealthBar.value = health
+	var tween = get_tree().create_tween()
+	tween.tween_property($CanvasLayer/HealthBar, "modulate:a", 1, 0.25)
 	$SummonAnim.play("fadein")
 	await $SummonAnim.animation_finished
 
@@ -8,7 +12,26 @@ func destroy():
 	set_deferred("monitorable", false)
 	set_deferred("monitoring", false)
 	set_physics_process(false)
+	var tween = get_tree().create_tween()
+	tween.tween_property($CanvasLayer/HealthBar, "modulate:a", 0, 0.25)
 	$SlimeSprite/SlimeParticles.emitting = true
 	$SummonAnim.play_backwards("fadein")
 	await $SummonAnim.animation_finished
 	queue_free()
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("PlayerBullets"):
+		$Hit.pitch_scale = randf_range(0.95, 1)
+		$Hit.play()
+		# modulate flash trick taken from
+		# https://www.reddit.com/r/godot/comments/y8n1wa/is_it_possible_to_make_a_sprite_flash_white_using/
+		$SlimeSprite.modulate.v = 3
+		if tween:
+			tween.kill()
+		tween = get_tree().create_tween()
+		tween.tween_property($SlimeSprite, "modulate:v", 1, 0.2)
+		area.destroy()
+		health -= area.damage
+		$CanvasLayer/HealthBar.value = health
+		if health <= 0:
+			destroy()
